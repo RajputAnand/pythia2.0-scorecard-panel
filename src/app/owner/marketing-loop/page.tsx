1046c11ab7f308ca4c49ba9c8300ae76a10d5edb
RajputAnand <anand.r@inheritx.com>
2026-08-14 10:49:30 +0530

    feat: implement add benchmarking API and demographic dashboard components

--- src/app/owner/marketing-loop/page.tsx
+++ src/app/owner/marketing-loop/page.tsx
@@ -2,10 +2,9 @@ import Header from '@/components/shared/Header/Header'
 import headerStyles from '@/components/shared/Header/Header.module.css'
 import AddCampaignButton from '@/components/AddCampaignButton/AddCampaignButton'
 import MarketingInsightStrip from '@/components/MarketingInsightStrip/MarketingInsightStrip'
-import DemographicShifts from '@/components/DemographicShifts/DemographicShifts'
-import CustomerSegmentShifts from '@/components/CustomerSegmentShifts/CustomerSegmentShifts'
 import SpendVsTraffic from '@/components/SpendVsTraffic/SpendVsTraffic'
 import CampaignCards from '@/components/CampaignCards/CampaignCards'
+
 export default async function MarketingLoopPage() {
   return (
     <>
@@ -16,12 +15,6 @@ export default async function MarketingLoopPage() {
 
       <div className="grid px-[30px] py-[24px] gap-5">
         <MarketingInsightStrip />
-
-        <div className="grid grid-cols-[1fr_1fr] gap-[18px] items-start">
-          <DemographicShifts />
-          <CustomerSegmentShifts />
-        </div>
-
         <SpendVsTraffic />
         <CampaignCards />
       </div>
