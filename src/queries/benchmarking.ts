7f2d4b74b4db882798239931ab21bf386a17558f
RajputAnand <anand.r@inheritx.com>
2026-08-14 12:39:38 +0530

    Revert "Revert "feat: implement add benchmarking API and demographic dashboard components""
    This reverts commit 4270375792e5bf00072d44477729226da84cc266.

---
+++ src/queries/benchmarking.ts
@@ -0,0 +1,26 @@
+import { pythia2Client } from '@/lib/api-client'
+import { PYTHIA_2_API } from '@/utils/api-endpoints'
+import {
+  BenchmarkingAllStoreDataResponse,
+  BenchmarkingFetchStoreDataResponse,
+} from '@/types/benchmarking'
+
+export async function fetchAllStoreData(token: string) {
+  const { data } = await pythia2Client.get<BenchmarkingAllStoreDataResponse>(
+    PYTHIA_2_API.benchmarking.allStoreData,
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  )
+  return data.data
+}
+
+export async function fetchStoreData(token: string, storeId: string) {
+  const { data } = await pythia2Client.get<BenchmarkingFetchStoreDataResponse>(
+    PYTHIA_2_API.benchmarking.fetchStoreData(storeId),
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  )
+  return data.data[0] || null
+}
